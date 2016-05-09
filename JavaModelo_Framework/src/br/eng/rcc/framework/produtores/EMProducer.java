9f235e4ee5b97ce44eb8b2279a192e8b664b22c8
rcheruti <rcheruti@WGPH_WS4.wgph.com.br>
2016-05-09 15:07:45 -0300

    Sombras

--- JavaModelo_Framework/src/br/eng/rcc/framework/produtores/EMProducer.java
+++ JavaModelo_Framework/src/br/eng/rcc/framework/produtores/EMProducer.java
@@ -18,7 +18,7 @@ import javax.persistence.PersistenceUnit;
 @ApplicationScoped
 public class EMProducer {
   
-  //@PersistenceUnit
+  @PersistenceUnit
   private EntityManagerFactory emf;
   private boolean myEMF;
   
@@ -40,10 +40,11 @@ public class EMProducer {
     System.out.printf("---  @Disposes EntityManager \n");
     try{
       if( em.isOpen() ){
-          em.flush();
-          if( em.getTransaction().isActive() ){
-            em.getTransaction().commit();
-          }
+        em.flush();
+        if( em.getTransaction().isActive() ){
+          em.getTransaction().commit();
+        }
+        em.clear();
         em.close();
       }
     }catch(IllegalStateException ex){
@@ -56,17 +57,21 @@ public class EMProducer {
   public void postContruct(){
     System.out.printf("---  EMProducer: postContruct. \n");
     
-    if( Configuracoes.persistenceUnit == null || Configuracoes.persistenceUnit.isEmpty() ){
-      
-      throw new RuntimeException("Configure o nome da PU em 'web.xml', no parametro 'Configuracoes.persistenceUnit'.");
+    if( Configuracoes.persistenceUnit != null && !Configuracoes.persistenceUnit.isEmpty() ){
+      System.out.printf("---  Criando novo EMF com a PU: %s \n", Configuracoes.persistenceUnit );
+      myEMF = true;
+      emf = Persistence.createEntityManagerFactory(Configuracoes.persistenceUnit);
+      //
     }
-    System.out.printf("---  Criando novo EMF com a PU: %s \n", Configuracoes.persistenceUnit );
-    myEMF = true;
-    emf = Persistence.createEntityManagerFactory(Configuracoes.persistenceUnit);
+    if( emf == null ){
+      throw new RuntimeException("Não encontramos uma PU! Configure o nome da PU em 'web.xml', no parametro 'Configuracoes.persistenceUnit'.");
+    }
+    
   }
   @PreDestroy
   public void preDestroy(){
     System.out.printf("---  EMProducer: preDestroy. \n");
+    emf.getCache().evictAll();
     if( myEMF && emf.isOpen() ){
       System.out.printf("---  Fechando EMF. \n");
       emf.close();
